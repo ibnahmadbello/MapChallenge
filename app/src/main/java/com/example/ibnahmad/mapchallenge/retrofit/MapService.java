3c54f94c78dd148998380b59edb984e7544caece
Ibn Ahmad <justforlearningcode@gmail.com>
2019-03-29 12:36:59 +0100

    MainActivity layout

---
+++ app/src/main/java/com/example/ibnahmad/mapchallenge/retrofit/MapService.java
@@ -0,0 +1,19 @@
+package com.example.ibnahmad.mapchallenge.retrofit;
+
+import com.example.ibnahmad.mapchallenge.pojo.ConsolidatedWeather;
+import com.example.ibnahmad.mapchallenge.pojo.Location;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+
+public interface MapService {
+
+    @GET("api/location/search/?")
+    Call<Location> getCityList(@Query("query") String query);
+
+    @GET("api/location/{woeid}")
+    Call<ConsolidatedWeather> getCityDetail(@Path("woeid") int woeid);
+
+}
