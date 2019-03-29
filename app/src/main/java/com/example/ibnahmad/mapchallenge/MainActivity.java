f7582b769d481f8a717b5b40f0067c9812f67258
ibnahmad <justforlearningcode@gmail.com>
2019-03-29 10:58:00 +0100

    Added search menu

--- app/src/main/java/com/example/ibnahmad/mapchallenge/MainActivity.java
+++ app/src/main/java/com/example/ibnahmad/mapchallenge/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.ibnahmad.mapchallenge;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.search_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        return super.onOptionsItemSelected(item);
     }
 }
